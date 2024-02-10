d94a87b13f11ee87da6209b1094f1c772a56cac0
Arthur Baroi <arthurcbaroi@hotmail.com>
2024-02-10 14:08:37 -0600

    Registering Products Module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductsModule } from './products/products.module';
 
 @NgModule({
-  declarations: [
+  declarations:
+  [
     AppComponent
   ],
-  imports: [
-    BrowserModule
+  imports:
+  [
+    BrowserModule,
+    ProductsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap:
+  [
+    AppComponent
+  ]
 })
 export class AppModule { }
