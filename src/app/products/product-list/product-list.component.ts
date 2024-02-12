cf83e5790aa3b5a9d75531bf0ea12d739fe1828e
Arthur Baroi <arthurcbaroi@hotmail.com>
2024-02-12 00:19:13 -0600

    Added Product Detail as a ViewChild and implemented the AfterViewInit interface.

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { ProductDetailComponent } from '../product-detail/product-detail.component';
 
 @Component({
   selector: 'app-product-list',
@@ -6,7 +7,16 @@ import { Component } from '@angular/core';
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent
+export class ProductListComponent implements AfterViewInit
 {
   selectedProduct = "";
+  @ViewChild(ProductDetailComponent) productDetail: ProductDetailComponent | undefined;
+
+  ngAfterViewInit(): void
+  {
+    if (this.productDetail)
+    {
+      console.log(this.productDetail.name);
+    }
+  }
 }
