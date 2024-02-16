54960859efaadb7c02e1f8741991db3c1ffb0f56
Arthur Baroi <arthurcbaroi@hotmail.com>
2024-02-16 17:06:53 -0600

    Added Favorites Component.

---
+++ src/app/products/favorites/favorites.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favorites',
+  templateUrl: './favorites.component.html',
+  styleUrl: './favorites.component.css'
+})
+
+export class FavoritesComponent
+{
+
+}
