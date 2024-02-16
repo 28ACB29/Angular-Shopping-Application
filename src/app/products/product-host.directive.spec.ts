4b473dd7de92ef184a1a39050ea4b435589e7224
Arthur Baroi <arthurcbaroi@hotmail.com>
2024-02-16 11:47:13 -0600

    Added Product Host Directive.

---
+++ src/app/products/product-host.directive.spec.ts
@@ -0,0 +1,10 @@
+import { ProductHostDirective } from './product-host.directive';
+
+describe('ProductHostDirective', () =>
+{
+  it('should create an instance', () =>
+  {
+    const directive = new ProductHostDirective();
+    expect(directive).toBeTruthy();
+  });
+});
