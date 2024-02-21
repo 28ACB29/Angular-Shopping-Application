d629947ea667a8f756a78f9834b717e118bdadaa
Arthur Baroi <arthurcbaroi@hotmail.com>
2024-02-21 13:18:27 -0600

    Added Product View Service.

---
+++ src/app/products/product-view/product-view.service.spec.ts
@@ -0,0 +1,19 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ProductViewService } from './product-view.service';
+
+describe('ProductViewService', () =>
+{
+  let service: ProductViewService;
+
+  beforeEach(() =>
+  {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ProductViewService);
+  });
+
+  it('should be created', () =>
+  {
+    expect(service).toBeTruthy();
+  });
+});
