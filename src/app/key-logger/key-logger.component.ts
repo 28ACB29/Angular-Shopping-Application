a92ace9a82bcb57f6061fbf87c7d9fbfb9d28e6b
Arthur Baroi <arthurcbaroi@hotmail.com>
2024-04-03 12:32:21 -0500

    Created Key Logger Component.

---
+++ src/app/key-logger/key-logger.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-key-logger',
+  standalone: true,
+  imports: [],
+  templateUrl: './key-logger.component.html',
+  styleUrl: './key-logger.component.css'
+})
+
+export class KeyLoggerComponent
+{
+
+}
