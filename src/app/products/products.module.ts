c6c45cf06e95f7a00d4547946fed498b4817c4f2
Arthur Baroi <arthurcbaroi@hotmail.com>
2024-04-04 11:37:25 -0500

    Registered Key Logger Component in Product Module.

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { KeyLoggerComponent } from '../key-logger/key-logger.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { SortPipe } from './sort.pipe';
-import { FavoritesComponent } from './favorites/favorites.component';
 import { ProductViewComponent } from './product-view/product-view.component';
+import { SortPipe } from './sort.pipe';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { ProductViewComponent } from './product-view/product-view.component';
     ProductDetailComponent,
     SortPipe,
     FavoritesComponent,
-    ProductViewComponent
+    ProductViewComponent,
+    KeyLoggerComponent
   ],
   imports:
   [
