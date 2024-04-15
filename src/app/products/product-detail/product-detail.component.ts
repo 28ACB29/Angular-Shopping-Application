2d0817b5761f93900b547afa260b471c7d564305
Arthur Baroi <arthurcbaroi@hotmail.com>
2024-04-15 15:44:06 -0500

    Import Observable, and add new fields in Product Detail Component.

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
+import { Observable } from 'rxjs';
+
 import { Product } from '../product';
 
 @Component({
@@ -6,11 +8,14 @@ import { Product } from '../product';
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
+
 export class ProductDetailComponent implements OnChanges
 {
   @Input() name = "";
   @Output() bought = new EventEmitter();
   @Input() product: Product | undefined;
+  @Input() id = -1;
+  product$: Observable<Product> | undefined;
 
   ngOnChanges(changes: SimpleChanges): void
   {
