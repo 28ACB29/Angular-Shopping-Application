e3ff1ef5fa5176fd967d518665f4ffdd6bd12586
Arthur Baroi <arthurcbaroi@hotmail.com>
2024-04-17 09:20:48 -0500

    Removed standalone Attribute from Product Create Component.

--- src/app/products/product-create/product-create.component.ts
+++ src/app/products/product-create/product-create.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-product-create',
-  standalone: true,
-  imports: [],
   templateUrl: './product-create.component.html',
   styleUrl: './product-create.component.css'
 })
