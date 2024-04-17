5878496758ac6569630cdf64deb59397a4a0f25a
Arthur Baroi <arthurcbaroi@hotmail.com>
2024-04-17 09:25:25 -0500

    Implemented Product Create Component.

--- src/app/products/product-create/product-create.component.ts
+++ src/app/products/product-create/product-create.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+import { Product } from '../product';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-product-create',
@@ -8,5 +11,20 @@ import { Component } from '@angular/core';
 
 export class ProductCreateComponent
 {
+
+  @Output() added = new EventEmitter<Product>();
+
+  constructor(private productsService: ProductsService)
+  {
+
+  }
+
+  createProduct(name: string, price: number)
+  {
+    this.productsService.addProduct(name, price).subscribe(product =>
+    {
+      this.added.emit(product);
+    });
+  }
 
 }
