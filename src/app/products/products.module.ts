e8ca0cfcd265529456028f90265b8fe46adc6f17
Arthur Baroi <arthurcbaroi@hotmail.com>
2024-04-25 12:12:58 -0500

    Added Missing Product Create Component import and sorted imports.

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { FavoritesComponent } from './favorites/favorites.component';
 import { KeyLoggerComponent } from '../key-logger/key-logger.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductCreateComponent } from './product-create/product-create.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product-list/product-list.component';
 import { ProductViewComponent } from './product-view/product-view.component';
 import { SortPipe } from './sort.pipe';
 
@@ -11,12 +13,13 @@ import { SortPipe } from './sort.pipe';
 @NgModule({
   declarations:
   [
-    ProductListComponent,
-    ProductDetailComponent,
-    SortPipe,
     FavoritesComponent,
+    KeyLoggerComponent,
+    ProductCreateComponent,
+    ProductDetailComponent,
+    ProductListComponent,
     ProductViewComponent,
-    KeyLoggerComponent
+    SortPipe,
   ],
   imports:
   [
