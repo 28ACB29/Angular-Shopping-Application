de89884dcdec037b749036b60aa4564dadcc08a5
Arthur Baroi <arthurcbaroi@hotmail.com>
2024-05-20 17:01:38 -0500

    Registered Product Service and Auth Service  and added changePrice and remove mthods in Product Detail Component.

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from
 import { Observable } from 'rxjs';
 
 import { Product } from '../product';
+import { ProductsService } from '../products.service';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -13,23 +15,39 @@ export class ProductDetailComponent implements OnChanges
 {
   @Input() name = "";
   @Output() bought = new EventEmitter();
+  @Output() deleted = new EventEmitter();
   @Input() product: Product | undefined;
   @Input() id = -1;
   product$: Observable<Product> | undefined;
 
+  constructor(private productService: ProductsService, public authService: AuthService)
+  {
+    
+  }
+
   ngOnChanges(changes: SimpleChanges): void
   {
-    const product = changes['product'];
-    if (!product.isFirstChange())
-    {
-      const oldValue = product.previousValue.name;
-      const newValue = product.currentValue.name;
-      console.log(`Product changed from ${oldValue} to ${newValue}`);
-    }
+    this.product$ = this.productService.getProduct(this.id);
   }
 
   buy()
   {
     this.bought.emit();
+  }
+
+  changePrice(product: Product, price: number)
+  {
+    this.productService.updateProduct(product.id, price).subscribe(() =>
+    {
+      alert(`The price of ${product.name} was changed!`);
+    });
+  }
+
+  remove(product: Product)
+  {
+    this.productService.deleteProduct(product.id).subscribe(() =>
+    {
+      this.deleted.emit();
+    });
   }
 }
