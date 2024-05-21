7ea9c3c758f51a5cf5bd119b61a84ecb76dea76f
Arthur Baroi <arthurcbaroi@hotmail.com>
2024-05-21 15:23:43 -0500

    Capitalized AuthInterceptor.

--- src/app/auth.interceptor.spec.ts
+++ src/app/auth.interceptor.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpInterceptorFn } from '@angular/common/http';
 
-import { authInterceptor } from './auth.interceptor';
+import { AuthInterceptor } from './auth.interceptor';
 
 describe('authInterceptor', () =>
 {
   const interceptor: HttpInterceptorFn = (req, next) => 
-    TestBed.runInInjectionContext(() => authInterceptor(req, next));
+    TestBed.runInInjectionContext(() => AuthInterceptor(req, next));
 
   beforeEach(() =>
   {
