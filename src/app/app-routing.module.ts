b3c7df1fd7224d472697a769f901de83e680f4de
Arthur Baroi <arthurcbaroi@hotmail.com>
2024-06-13 13:47:11 -0500

    Fixed formatting in App Routing Module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,8 +21,14 @@ const routes: Routes =
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports:
+  [
+    RouterModule.forRoot(routes)
+  ],
+  exports:
+  [
+    RouterModule
+  ]
 })
 
 export class AppRoutingModule
