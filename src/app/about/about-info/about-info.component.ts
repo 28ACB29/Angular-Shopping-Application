6fc803cef03fc1baaae5075a6612e5875a25f683
Arthur Baroi <arthurcbaroi@hotmail.com>
2024-07-12 12:34:54 -0500

    Created About Component.

---
+++ src/app/about/about-info/about-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-info',
+  templateUrl: './about-info.component.html',
+  styleUrl: './about-info.component.css'
+})
+
+export class AboutInfoComponent
+{
+
+}
