3a19e954546cc1221df2f22484c4399a4afd5ae2
Arthur Baroi <arthurcbaroi@hotmail.com>
2024-07-29 15:59:48 -0500

    Updated Porduct Create Component to use forms and Price Range Directive.

--- src/app/products/product-create/product-create.component.ts
+++ src/app/products/product-create/product-create.component.ts
@@ -1,7 +1,9 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { Product } from '../product';
 import { ProductsService } from '../products.service';
+import { priceRangeValidator } from '../price-range.directive';
 
 @Component({
   selector: 'app-product-create',
@@ -13,6 +15,29 @@ export class ProductCreateComponent
 {
 
   @Output() added = new EventEmitter<Product>();
+  productForm = new FormGroup({
+    name: new FormControl('',
+    {
+      nonNullable: true,
+      validators: Validators.required
+    }),
+    price: new FormControl<number | undefined>(undefined,
+    {
+      nonNullable: true,
+      validators: [Validators.required, priceRangeValidator()]
+    })
+  });
+  showPriceRangeHint = false;
+
+  get name()
+  {
+    return this.productForm.controls.name
+  }
+  
+  get price()
+  {
+    return this.productForm.controls.price
+  }
 
   constructor(private productsService: ProductsService)
   {
@@ -21,8 +46,9 @@ export class ProductCreateComponent
 
   createProduct(name: string, price: number)
   {
-    this.productsService.addProduct(name, price).subscribe(product =>
+    this.productsService.addProduct(this.name.value, Number(this.price.value)).subscribe(product =>
     {
+      this.productForm.reset();
       this.added.emit(product);
     });
   }
