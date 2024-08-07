0cfeb53a5a18bc3029bf45714c69e1f53d3ea1b4
Arthur Baroi <arthurcbaroi@hotmail.com>
2024-08-07 16:08:29 -0500

    Added Cart Form Form Group  to Cart Component.

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
+
+import { Product } from '../products/product';
 
 @Component({
   selector: 'app-cart',
@@ -8,5 +11,11 @@ import { Component } from '@angular/core';
 
 export class CartComponent
 {
+
+  cartForm = new FormGroup({
+    products: new FormArray<FormControl<number>>([])
+  });
+  cart: Product[] = [];
+
 
 }
