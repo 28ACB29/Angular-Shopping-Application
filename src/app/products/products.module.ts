29aaa8de47d5951e6edf53e291a44cb4db584a6e
Arthur Baroi <arthurcbaroi@hotmail.com>
2024-08-15 12:41:11 -0500

    Imported Angular Material Components into Products Module.

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,6 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 import { FavoritesComponent } from './favorites/favorites.component';
 import { KeyLoggerComponent } from '../key-logger/key-logger.component';
@@ -26,6 +39,19 @@ import { SortPipe } from './sort.pipe';
   [
     CommonModule,
     FormsModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatInputModule,
+    MatListModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatSelectModule,
+    MatSortModule,
+    MatTableModule,
     ReactiveFormsModule,
     ProductsRoutingModule
   ],
