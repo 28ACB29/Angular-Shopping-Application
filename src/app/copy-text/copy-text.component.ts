288458c5133001a3276d79233d6f346db98076d0
Arthur Baroi <arthurcbaroi@hotmail.com>
2024-09-10 10:39:45 -0500

    Created Copy Text Component.

---
+++ src/app/copy-text/copy-text.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-copy-text',
+  templateUrl: './copy-text.component.html',
+  styleUrl: './copy-text.component.css'
+})
+
+export class CopyTextComponent
+{
+
+}
